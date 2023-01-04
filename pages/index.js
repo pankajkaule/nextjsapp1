060da7878739dfb3e17713b46568d0a75ec8a2e5
pankajkaule <pankajkaule@gmail.com>
2023-01-04 18:42:23 +0530

    Home Page In Progress

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,30 @@
 import Footer from "./Component/Footer";
 import Navbar from "./Component/Navbar";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <Navbar/>
-      <div className="h-screen">fdg</div>
-      <Footer/>
+      <Navbar />
+      <div className="flex justify-center ">
+        <div class="container">
+          <img height={"400px"} width="100%" src="/assets/images/laptops.jpg" />
+          <div class="bottom-left lineLeft">
+          <p class="lineLeft text-4xl font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+      <br/>
+      <p class="lineLeft text-4xl font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
